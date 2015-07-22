cece575b88d801791dfe770bb9093a874aba6ee6
jangromko <gromkojan@gmail.com>
2015-07-22 11:50:35 +0200

    Komentarz

--- lib/batleth.ex
+++ lib/batleth.ex
@@ -1,31 +1,36 @@
-require Amnesia
-use Amnesia
-use Database
+
 
 
 defmodule Batleth do
 	use Application
 
 	def start(_type, _args) do
+		require Amnesia
+		use Amnesia
+
+		use Database
 		Mix.Task.run(:install, [])
                 Amnesia.start
 		IO.puts "Witaj swiecie"
-Amnesia.transaction do
-	IO.puts "Tran1"
-	r = %Wpis{timestamp: 55555, status: 2, pr: 87} 
-        Wpis.write r
-	IO.inspect(r)
-end
-Amnesia.start
-Amnesia.transaction do
-        IO.puts "Tran2"
-	r = Wpis.where timestamp == 55555
-	IO.inspect r
-end
+		Amnesia.transaction do
+			IO.puts "Tran1"
+			r = %Wpis{timestamp: 55555, status: 2, pr: 87} |> Wpis.write
+			IO.puts r.timestamp
+		end
 
-IO.puts "Hello World"
-	Mix.Task.run(:uninstall, [])
-{:ok, self()}
+		Amnesia.transaction do
+		        IO.puts "Tran2"
+			r = Wpis.where timestamp == 55555, select: status
+			IO.inspect r
+			IO.puts "Żegnaj świecie"
+			r |> Amnesia.Selection.values |> Enum.each &IO.puts(&1.status)
+		end
+
+
+
+		IO.puts "Hello World"
+		#Mix.Task.run(:uninstall, [])
+		{:ok, self()}
 end
 end
 
